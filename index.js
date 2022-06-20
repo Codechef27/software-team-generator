c06dc8643bbf0c62907bfddbd61a0894e1b2edf2
BradleyK <bradleyjkennett@gmail.com>
2022-06-20 16:41:25 -0400

    prompt questins add

--- index.js
+++ index.js
@@ -24,8 +24,138 @@
 
 // THEN I exit the application, and the HTML is generated
 
-const inquirer = require('inquirer');
+const inquirer = require('inquirer' );
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const managerPrompt = (managerData) => {
+    return inquirer.prompt([ 
+
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter your team manager's name?",
+            validate: name => {
+                if (name) {
+                    return true
+                } else {
+                    console.log('You must enter a name!');
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'employee id',
+            message: "What is your team manager's employee id number?",
+            validate: employeeId => {
+                if (employeeId) {
+                    return true
+                } else {
+                    console.log('You must enter a id number');
+                }
+            }
+
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter a email address for the team manager!'
+        },
+
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter the office number for the team manager'
+        }
+    ])
+};
+
+const addOrFinished = () => {
+    console.log(`
+    =======================
+       Add a Team Member
+    =======================
+      `);
+    return inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'teamMember',
+            message: 'Are you adding a Engineer a Intern or are you finished building your team?',
+            choices: ['Add a Engineer', 'Add a Intern', 'Finished!'],
+            default: 'finished'
+        }
+    ])
+
+}
+
+const addEngineer = () => {
+    console.log(`
+    =============
+      Engineer
+    =============  
+    `)
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the Engineer's name?",
+        },
+
+        {
+            type: 'input',
+            name: 'employee id',
+            message: "What is the Engineer's employee id number?"
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Engineer's email address?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the Engineer's Github username?"
+        }
+    ])
+};
+
+const addIntern = () => {
+    console.log(`
+    =============
+       Intern
+    =============  
+    `)
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the Intern's name?",
+        },
+
+        {
+            type: 'input',
+            name: 'employee id',
+            message: "What is the Intern's employee id number?"
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Intern's email address?"
+        },
+        {
+            type: 'input',
+            name: 'college',
+            message: "What college is the intern from?"
+        }
+    ])
+};
+
+
+managerPrompt()
+
 
